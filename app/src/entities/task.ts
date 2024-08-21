e38e45cbfff051f991545b667fad4ec12bb7fc11
Lucas Sena <lucasmelosena@gmail.com>
2024-08-21 18:29:17 -0300

    feat: create task use-cases, repository and entity

---
+++ app/src/entities/task.ts
@@ -0,0 +1,71 @@
+import { randomUUID } from 'crypto';
+import { Replace } from 'src/helpers/replace';
+
+export enum TaskStatus {
+  Pending = 'PENDING',
+  InProgress = 'IN_PROGRESS',
+  Done = 'DONE',
+}
+
+export interface TaskProps {
+  id?: string | null;
+  user_id: string;
+  title: string;
+  description: string;
+  status?: TaskStatus | null;
+  created_at?: Date | null;
+}
+
+export class Task {
+  private props: TaskProps;
+
+  constructor(
+    props: Replace<
+      TaskProps,
+      { id?: string; created_at?: Date; status?: TaskStatus }
+    >,
+  ) {
+    this.props = {
+      ...props,
+      id: props.id ?? randomUUID(),
+      created_at: props.created_at ?? new Date(),
+      status: props.status ?? TaskStatus.Pending,
+    };
+  }
+
+  public get id() {
+    return this.id;
+  }
+
+  public set user_id(user_id: string) {
+    this.props.user_id = user_id;
+  }
+
+  public get user_id(): string {
+    return this.props.user_id;
+  }
+
+  public set title(title: string) {
+    this.props.title = title;
+  }
+
+  public get title(): string {
+    return this.props.title;
+  }
+
+  public set description(description: string) {
+    this.props.description = description;
+  }
+
+  public get description(): string {
+    return this.props.description;
+  }
+
+  public get status(): TaskStatus {
+    return this.props.status;
+  }
+
+  public get created_at(): Date {
+    return this.props.created_at;
+  }
+}
---
+++ app/src/repositories/tasks.repository.ts
@@ -0,0 +1,8 @@
+import { Task } from 'src/entities/task';
+
+export abstract class TasksRepository {
+  abstract create(task: Task): Promise<Task>;
+  abstract findMany(user_id: string): Promise<Task[]>;
+  abstract update(task: Task): Promise<Task>;
+  abstract delete(id: string): Promise<string>;
+}
